553576b4d8970abfe4284d43f13b51682660e412
pedro.saldanac.est <pedro.saldanac.est@gmail.com>
2025-03-05 20:49:22 +0000

    main2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ai.P1425.app',
   appName: 'proyectoCameraE2',
-  webDir: 'dist'
+  webDir: 'dist/'
 };
 
 export default config;
